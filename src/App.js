207ae5b72e263a1253f96c2c4cd758258d467f10
virajj27 <93832504+virajj27@users.noreply.github.com>
2023-01-23 00:55:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import TestForm from "./TestForm";
 // import CampusAmbForm from "./CampusAmbForm";
 import TermsAndConditions from "./TermsAndConditions";
 import PrivacyPolicy from "./PrivacyPolicy";
-import RegClosed from "./RegClosed";
+// import RegClosed from "./RegClosed";
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
         <Route path="registerforvp" element={<TestForm />} />
         <Route path="/termsAndConditions" element={<TermsAndConditions />} />
         <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
-        <Route path="/closed" element={<RegClosed />} />
+//         <Route path="/closed" element={<RegClosed />} />
         <Route path="/" element={<TestForm />} />
       </Routes>
     </div>
